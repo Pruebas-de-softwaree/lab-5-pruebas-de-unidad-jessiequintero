b91b7b9e5d633287b0f13813a77eec1487fa5449
jessiequintero <113272508+jessiequintero@users.noreply.github.com>
2025-10-03 03:02:55 +0000

    Tests unitarios

--- book.py
+++ book.py
@@ -16,4 +16,15 @@ class Book:
             raise ValueError("The book is not currently borrowed")
         self.borrowed = False
 
+if __name__ == "__main__":
+    book1 = Book("2010", "Jackie Quint", 2008, "ISBN003")
+
+    #Test_U01
+    book1_borrow = book1.borrow()
+    print(book1.borrowed)
+    
+    #Test_U02
+    book1_return = book1.return_book()
+    print(book1.borrowed)
+
 
--- user.py
+++ user.py
@@ -5,10 +5,18 @@ class User:
         self.history = []
 
     def register_borrow(self, book):
-        self.history.append(f"Borrwed: {book.title}")
+        self.history.append(f"Borrwed: {book}")
 
     def register_return(self, book):
-        self.history.append(f"Reurned: {book.title}")
+        self.history.append(f"Reurned: {book}")
 
     def get_history(self):
         return self.history
+
+if __name__ == "__main__":
+    user1 = User("Jessica", 1)
+
+    #Test_U03 y Test_U04
+    user1.register_borrow("El principito")
+    user1_history = user1.get_history()
+    print(user1_history)
